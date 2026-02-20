16ad2049a4c770bb42e8aafc728b523a75df4610
warsakeeb <aloneboyofficial79@gmail.com>
2026-02-20 17:19:55 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
-// This ultra-simple worker just exists to pass Chrome's PWA check.
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
-self.addEventListener('fetch', (e) => {});
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  // Purely a pass-through to satisfy Chrome's PWA requirement
+});
