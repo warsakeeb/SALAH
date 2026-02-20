f09dca50b3daef1b67ee1c1300bd4cd11f1c7ba7
warsakeeb <aloneboyofficial79@gmail.com>
2026-02-20 16:28:37 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,20 @@
-// I changed this to v2. This forces your phone to update the app!
-const CACHE_NAME = 'war-sakeeb-cache-v2';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.png',
-  './adhan.mp3' 
-];
-
-self.addEventListener('install', event => {
-  // Force the new worker to activate immediately
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Delete old v1 cache so it doesn't get stuck anymore
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
+{
+  "name": "WAR SAKEEB Tracker",
+  "short_name": "WAR SAKEEB",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#f1f5f9",
+  "icons": [
+    {
+      "src": "https://cdn-icons-png.flaticon.com/192/713/713302.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/713/713302.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
