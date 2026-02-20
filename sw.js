2d64f75e9e6ba34fe17c95cfcf7209fa8f0dbd81
warsakeeb <aloneboyofficial79@gmail.com>
2026-02-20 16:04:39 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,36 @@
-const CACHE_NAME = 'war-sakeeb-cache-v1';
+// I changed this to v2. This forces your phone to update the app!
+const CACHE_NAME = 'war-sakeeb-cache-v2';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './Adhan.mp3' 
+  './icon.png',
+  './adhan.mp3' 
 ];
 
 self.addEventListener('install', event => {
+  // Force the new worker to activate immediately
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
+  );
+});
+
+// Delete old v1 cache so it doesn't get stuck anymore
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
 
